18240a782a7a035b0acee6dadd1ed3c618a9f2b6
rickyleonardowijaya <58543495+rickyleonardowijaya@users.noreply.github.com>
2019-12-05 15:27:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -53,32 +53,32 @@
             echo "Failed: " . $e;
         }
         echo "<h3>Your're registered!</h3>";
-     }// else if (isset($_POST['load_data'])) {
-    //     try {
-    //         //$sql_select = "SELECT * FROM User";
-    //         $stmt = $conn->query($sql_select);
-    //         $registrants = $stmt->fetchAll(); 
-    //         if(count($registrants) > 0) {
-    //             echo "<h2>People who are registered:</h2>";
-    //             echo "<table>";
-    //             echo "<tr><th>Name</th>";
-    //             echo "<th>Email</th>";
-    //             echo "<th>Job</th>";
-    //             echo "<th>Date</th></tr>";
-    //             foreach($registrants as $registrant) {
-    //                 echo "<tr><td>".$registrant['name']."</td>";
-    //                 echo "<td>".$registrant['email']."</td>";
-    //                 echo "<td>".$registrant['job']."</td>";
-    //                 echo "<td>".$registrant['date']."</td></tr>";
-    //             }
-    //             echo "</table>";
-    //         } else {
-    //             echo "<h3>No one is currently registered.</h3>";
-    //         }
-    //     } catch(Exception $e) {
-    //         echo "Failed: " . $e;
-    //     }
-    // }
+     } else if (isset($_POST['load_data'])) {
+        try {
+            //$sql_select = "SELECT * FROM User";
+            $stmt = $conn->query($sql_select);
+            $registrants = $stmt->fetchAll(); 
+            if(count($registrants) > 0) {
+                echo "<h2>People who are registered:</h2>";
+                echo "<table>";
+                echo "<tr><th>Name</th>";
+                echo "<th>Email</th>";
+                echo "<th>Job</th>";
+                echo "<th>Date</th></tr>";
+                foreach($registrants as $registrant) {
+                    echo "<tr><td>".$registrant['name']."</td>";
+                    echo "<td>".$registrant['email']."</td>";
+                    echo "<td>".$registrant['job']."</td>";
+                    echo "<td>".$registrant['date']."</td></tr>";
+                }
+                echo "</table>";
+            } else {
+                echo "<h3>No one is currently registered.</h3>";
+            }
+        } catch(Exception $e) {
+            echo "Failed: " . $e;
+        }
+    }
  ?>
  </body>
  </html>
