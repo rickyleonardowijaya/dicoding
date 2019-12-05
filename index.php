5d4f0db22d6e464f3e7c0d106e9c002b24f3d3ef
rickyleonardowijaya <58543495+rickyleonardowijaya@users.noreply.github.com>
2019-12-05 22:48:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
  <form method="post" action="index.php" enctype="multipart/form-data" >
        Nama  <input type="text" name="nama" id="nama"/></br></br>
        Genre <input type="text" name="genre" id="genre"/></br></br>
-       Deskripsi <textarea name="deskripsi" id="deskripsi"></textarea></br></br>
+       Deskripsi <textarea rows="4" cols="50" name="deskripsi" id="deskripsi"></textarea></br></br>
        <input type="submit" name="simpan" value="Simpan" />
        <input type="submit" name="tampilData" value="Tampil Data" />
  </form>
