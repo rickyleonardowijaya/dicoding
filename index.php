451d109d9c18be27336d28f9055eb79180208297
rickyleonardowijaya <58543495+rickyleonardowijaya@users.noreply.github.com>
2019-12-07 00:57:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -147,14 +147,6 @@ if (isset($_GET["Cleanup"])) {
 </head>
 <body>
  
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Analyze Sample</title>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
-</head>
-<body>
- 
 <script type="text/javascript">
     function processImage() {
         // **********************************************
@@ -242,4 +234,5 @@ Image to analyze:
 </div>
 </body>
 </html>
+
 
