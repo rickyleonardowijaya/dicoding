42945b8899c04ff8ca6026b19937fdd09f9f93fe
rickyleonardowijaya <58543495+rickyleonardowijaya@users.noreply.github.com>
2019-12-07 02:13:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -118,7 +118,7 @@ if (isset($_GET["Cleanup"])) {
         .done(function(data) {
             // Show formatted JSON on webpage.
 		var json = JSON.stringify(data,null,2);
-            $("#responseTextArea").val(json[0]);
+            $("#responseTextArea").val(json[0].category);
 // 	    $("#responseTextArea").val(data[2].description[1].text);
         })
  
