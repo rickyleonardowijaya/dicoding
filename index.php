5ee0536d1b6b7c272d5834dd5d0a7af157fb8841
rickyleonardowijaya <58543495+rickyleonardowijaya@users.noreply.github.com>
2019-12-06 13:07:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -41,22 +41,38 @@ use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
 use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
 use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
 
+$connectionString = "DefaultEndpointsProtocol=https;AccountName=".getenv('ACCOUNT_NAME').";AccountKey=".getenv('ACCOUNT_KEY');
 
+// Create blob client.
+$blobClient = BlobRestProxy::createBlobService($connectionString);
 
-if (isset($_GET["upload"])) {
-    $connectionString = "DefaultEndpointsProtocol=https;AccountName=gamerlistenerapp;AccountKey=9HBvgsWyPqN+afxqSuELsdHJQPcI/kDfpd/amikDzmFReuw8CtDnm4klnp87ys5u9pQpcyHvl6+BZyF5yVY0BQ==;EndpointSuffix=core.windows.net";
+$fileToUpload = "HelloWorld.txt";
 
-    // Create blob client.
-    $blobClient = BlobRestProxy::createBlobService($connectionString);
-
+if (!isset($_GET["Cleanup"])) {
+    // Create container options object.
     $createContainerOptions = new CreateContainerOptions();
+
+    // Set public access policy. Possible values are
+    // PublicAccessType::CONTAINER_AND_BLOBS and PublicAccessType::BLOBS_ONLY.
+    // CONTAINER_AND_BLOBS:
+    // Specifies full public read access for container and blob data.
+    // proxys can enumerate blobs within the container via anonymous
+    // request, but cannot enumerate containers within the storage account.
+    //
+    // BLOBS_ONLY:
+    // Specifies public read access for blobs. Blob data within this
+    // container can be read via anonymous request, but container data is not
+    // available. proxys cannot enumerate blobs within the container via
+    // anonymous request.
+    // If this value is not specified in the request, container data is
+    // private to the account owner.
     $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
 
     // Set container metadata.
     $createContainerOptions->addMetaData("key1", "value1");
     $createContainerOptions->addMetaData("key2", "value2");
 
-      $containerName = "Submission";
+      $containerName = "blockblobs".generateRandomString();
 
     try {
         // Create container.
@@ -116,14 +132,28 @@ if (isset($_GET["upload"])) {
         echo $code.": ".$error_message."<br />";
     }
 } 
+else 
+{
+
+    try{
+        // Delete container.
+        echo "Deleting Container".PHP_EOL;
+        echo $_GET["containerName"].PHP_EOL;
+        echo "<br />";
+        $blobClient->deleteContainer($_GET["containerName"]);
+    }
+    catch(ServiceException $e){
+        // Handle exception based on error codes and messages.
+        // Error codes and messages are here:
+        // http://msdn.microsoft.com/library/azure/dd179439.aspx
+        $code = $e->getCode();
+        $error_message = $e->getMessage();
+        echo $code.": ".$error_message."<br />";
+    }
+}
 ?>
 
-<p>Pilih gambar yang akan di Analisa</p>
-    <form action="index.php" method="post">
-        <input type="file" name="image" id="image"><br><br>
-        <input type="submit" value="Upload" name="upload" class="btn btn-primary">
-    </form>
 
-<form method="post" action="index.php?Cleanup&containerName=<?php echo $containerName; ?>">
+<form method="post" action="phpQS.php?Cleanup&containerName=<?php echo $containerName; ?>">
     <button type="submit">Press to clean up all resources created by this sample</button>
 </form>
