1fc56bad7ea8ea63b682ce9ba3497c34247ba7a9
rickyleonardowijaya <58543495+rickyleonardowijaya@users.noreply.github.com>
2019-12-07 01:37:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -84,7 +84,7 @@ if (isset($_GET["Cleanup"])) {
  
        
         var uriBase =
-            "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/description";
+            "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/Description";
  
         // Request parameters.
         var params = {
