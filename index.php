a8c6bf1a989ddfb8f4f4d86fa8c2f80ba3757667
rickyleonardowijaya <58543495+rickyleonardowijaya@users.noreply.github.com>
2019-12-06 13:24:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -46,7 +46,7 @@ $connectionString = "DefaultEndpointsProtocol=https;AccountName=gamerlistenerapp
 // Create blob client.
 $blobClient = BlobRestProxy::createBlobService($connectionString);
 
-$fileToUpload = "HelloWorld.txt";
+$fileToUpload = "HelloWorld123.txt";
 
 if (!isset($_GET["Cleanup"])) {
     // Create container options object.
