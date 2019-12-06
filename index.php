e6275b49e4fbf9585c12487ca2183c823e949d11
rickyleonardowijaya <58543495+rickyleonardowijaya@users.noreply.github.com>
2019-12-06 12:21:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -90,7 +90,7 @@ if (!isset($_GET["Cleanup"])) {
         $content = fopen($fileToUpload, "r");
 
         //Upload blob
-        $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
+//         $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
 
         // List blobs.
         $listBlobsOptions = new ListBlobsOptions();
@@ -98,22 +98,22 @@ if (!isset($_GET["Cleanup"])) {
 
         echo "These are the blobs present in the container: ";
 
-        do{
-            $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
-            foreach ($result->getBlobs() as $blob)
-            {
-                echo $blob->getName().": ".$blob->getUrl()."<br />";
-            }
+//         do{
+//             $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
+//             foreach ($result->getBlobs() as $blob)
+//             {
+//                 echo $blob->getName().": ".$blob->getUrl()."<br />";
+//             }
         
-            $listBlobsOptions->setContinuationToken($result->getContinuationToken());
-        } while($result->getContinuationToken());
-        echo "<br />";
+//             $listBlobsOptions->setContinuationToken($result->getContinuationToken());
+//         } while($result->getContinuationToken());
+//         echo "<br />";
 
-        // Get blob.
-        echo "This is the content of the blob uploaded: ";
-        $blob = $blobClient->getBlob($containerName, $fileToUpload);
-        fpassthru($blob->getContentStream());
-        echo "<br />";
+//         // Get blob.
+//         echo "This is the content of the blob uploaded: ";
+//         $blob = $blobClient->getBlob($containerName, $fileToUpload);
+//         fpassthru($blob->getContentStream());
+//         echo "<br />";
     }
     catch(ServiceException $e){
         // Handle exception based on error codes and messages.
