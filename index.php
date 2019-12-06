14146162101cc9f87f325e999d0b0b203bbf1694
rickyleonardowijaya <58543495+rickyleonardowijaya@users.noreply.github.com>
2019-12-07 00:45:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -165,7 +165,7 @@ if (isset($_GET["Cleanup"])) {
         // If you use a free trial subscription key, you shouldn't need to change
         // this region.
         var uriBase =
-            "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
+            "https://southeastasia.api.cognitive.microsoft.com/";
  
         // Request parameters.
         var params = {
