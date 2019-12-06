9157413d21525c80740dcce0c9c234ca2937826c
rickyleonardowijaya <58543495+rickyleonardowijaya@users.noreply.github.com>
2019-12-07 01:13:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -175,6 +175,7 @@ if (isset($_GET["Cleanup"])) {
  
         // Display the image.
         var sourceImageUrl = document.getElementById("imageanalisis").value;
+	    console.log(sourceImageUrl);
         document.querySelector("#sourceImage").src = sourceImageUrl;
  
         // Make the REST API call.
