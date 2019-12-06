491084aea034bdf4d1ef67a997338d835a687523
rickyleonardowijaya <58543495+rickyleonardowijaya@users.noreply.github.com>
2019-12-06 23:21:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -87,7 +87,7 @@ if (isset($_GET["Cleanup"])) {
 	    	echo "GAGA";
 	    }
 	    
-	    $myfile = fopen($fileToUpload, "w") or die("Unable to open file!");
+	    $myfile = fopen($fileToUpload, "r") or die("Unable to open file!");
 
         fclose($myfile);
 
