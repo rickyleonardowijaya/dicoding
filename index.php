502bdb3ac466060a1fdc50bcc73933998adc4125
rickyleonardowijaya <58543495+rickyleonardowijaya@users.noreply.github.com>
2019-12-07 01:25:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,4 @@
 <?php
-/**----------------------------------------------------------------------------------
-* Microsoft Developer & Platform Evangelism
-*
-* Copyright (c) Microsoft Corporation. All rights reserved.
-*
-* THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
-* EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES 
-* OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
-*----------------------------------------------------------------------------------
-* The example companies, organizations, products, domain names,
-* e-mail addresses, logos, people, places, and events depicted
-* herein are fictitious.  No association with any real company,
-* organization, product, domain name, email address, logo, person,
-* places, or events is intended or should be inferred.
-*----------------------------------------------------------------------------------
-**/
-/** -------------------------------------------------------------
-# Azure Storage Blob Sample - Demonstrate how to use the Blob Storage service. 
-# Blob storage stores unstructured data such as text, binary data, documents or media files. 
-# Blobs can be accessed from anywhere in the world via HTTP or HTTPS. 
-#
-# Documentation References: 
-#  - Associated Article - https://docs.microsoft.com/en-us/azure/storage/blobs/storage-quickstart-blobs-php 
-#  - What is a Storage Account - http://azure.microsoft.com/en-us/documentation/articles/storage-whatis-account/ 
-#  - Getting Started with Blobs - https://azure.microsoft.com/en-us/documentation/articles/storage-php-how-to-use-blobs/
-#  - Blob Service Concepts - http://msdn.microsoft.com/en-us/library/dd179376.aspx 
-#  - Blob Service REST API - http://msdn.microsoft.com/en-us/library/dd135733.aspx 
-#  - Blob Service PHP API - https://github.com/Azure/azure-storage-php
-#  - Storage Emulator - http://azure.microsoft.com/en-us/documentation/articles/storage-use-emulator/ 
-#
-**/
 require_once 'vendor/autoload.php';
 require_once "./random_string.php";
 use MicrosoftAzure\Storage\Blob\BlobRestProxy;
@@ -38,58 +7,37 @@ use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
 use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
 use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
 $connectionString = "DefaultEndpointsProtocol=https;AccountName=gamerlistenerapp;AccountKey=9HBvgsWyPqN+afxqSuELsdHJQPcI/kDfpd/amikDzmFReuw8CtDnm4klnp87ys5u9pQpcyHvl6+BZyF5yVY0BQ==;EndpointSuffix=core.windows.net";
-// Create blob client.
+
 $blobClient = BlobRestProxy::createBlobService($connectionString);
 $fileToUpload = "tes.png";
 if (isset($_GET["Cleanup"])) {
-    // Create container options object.
     $createContainerOptions = new CreateContainerOptions();
-    // Set public access policy. Possible values are
-    // PublicAccessType::CONTAINER_AND_BLOBS and PublicAccessType::BLOBS_ONLY.
-    // CONTAINER_AND_BLOBS:
-    // Specifies full public read access for container and blob data.
-    // proxys can enumerate blobs within the container via anonymous
-    // request, but cannot enumerate containers within the storage account.
-    //
-    // BLOBS_ONLY:
-    // Specifies public read access for blobs. Blob data within this
-    // container can be read via anonymous request, but container data is not
-    // available. proxys cannot enumerate blobs within the container via
-    // anonymous request.
-    // If this value is not specified in the request, container data is
-    // private to the account owner.
+    
     $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
-    // Set container metadata.
     $createContainerOptions->addMetaData("key1", "value1");
     $createContainerOptions->addMetaData("key2", "value2");
-      $containerName = "submission";
+    $containerName = "submission";
     try {
-       //  fopen($fileToUpload, "r");
-	        $fileToUpload = $_FILES['image']['name'];
-	     $targetdir = "img/";
-        $targetFile = basename($_FILES["image"]["name"]);
-        if(file_put_contents($targetFile, file_get_contents($_FILES['image']["tmp_name"]))){
+	   $fileToUpload = $_FILES['image']['name'];
+	   $targetdir = "img/";
+           $targetFile = basename($_FILES["image"]["name"]);
+           if(file_put_contents($targetFile, file_get_contents($_FILES['image']["tmp_name"]))){
 		echo "Sukses";
-	}
-	    else{
+           }
+	   else{
 	    	echo "GAGA";
-	    }
+	   }
 	    
 	    $myfile = fopen($fileToUpload, "w") or die("Unable to open file!");
-        fclose($myfile);
+            fclose($myfile);
         
         # Upload file as a block blob
-        echo "Uploading BlockBlob: ".PHP_EOL;
-        echo $fileToUpload;
-        echo "<br />";
+            echo "Uploading BlockBlob: ".PHP_EOL;
+            echo $fileToUpload;
+            echo "<br />";
         
-        $content = file_get_contents($_FILES['image']["tmp_name"]);
-        //Upload blob
-        $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
-	
-        //Upload blob
-//         $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
-//         List blobs.
+            $content = file_get_contents($_FILES['image']["tmp_name"]);
+            $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
 	    echo $fileToUpload;
         $listBlobsOptions = new ListBlobsOptions();
         $listBlobsOptions->setPrefix($fileToUpload);
@@ -101,7 +49,7 @@ if (isset($_GET["Cleanup"])) {
                 echo $blob->getName().": ".$blob->getUrl()."<br />";
 		echo "<p name='imageanalisis' id='imageanalisis'>".$blob->getUrl()."</p>";
 		echo "<input type='text' name='imageA' id='imageA'
-    value='".$blob->getUrl()."' />";
+                        value='".$blob->getUrl()."' />";
 		echo "<br/>";
 		echo "<img name='gambaranalisis' src='".$blob->getUrl()."' width='100' height='100'/>";
             }
@@ -109,24 +57,13 @@ if (isset($_GET["Cleanup"])) {
             $listBlobsOptions->setContinuationToken($result->getContinuationToken());
         } while($result->getContinuationToken());
         echo "<br />";
-        // Get blob.
-//         echo "This is the content of the blob uploaded: ";
-//         $blob = $blobClient->getBlob($containerName, $fileToUpload);
-//         fpassthru($blob->getContentStream());
-//         echo "<br />";
     }
     catch(ServiceException $e){
-        // Handle exception based on error codes and messages.
-        // Error codes and messages are here:
-        // http://msdn.microsoft.com/library/azure/dd179439.aspx
         $code = $e->getCode();
         $error_message = $e->getMessage();
         echo $code.": ".$error_message."<br />";
     }
     catch(InvalidArgumentTypeException $e){
-        // Handle exception based on error codes and messages.
-        // Error codes and messages are here:
-        // http://msdn.microsoft.com/library/azure/dd179439.aspx
         $code = $e->getCode();
         $error_message = $e->getMessage();
         echo $code.": ".$error_message."<br />";
@@ -149,21 +86,10 @@ if (isset($_GET["Cleanup"])) {
  
 <script type="text/javascript">
     function processImage() {
-        // **********************************************
-        // *** Update or verify the following values. ***
-        // **********************************************
- 
-        // Replace <Subscription Key> with your valid subscription key.
+       
         var subscriptionKey = "a8b47712688c4154818536360975762b";
  
-        // You must use the same Azure region in your REST API method as you used to
-        // get your subscription keys. For example, if you got your subscription keys
-        // from the West US region, replace "westcentralus" in the URL
-        // below with "westus".
-        //
-        // Free trial subscription keys are generated in the "westus" region.
-        // If you use a free trial subscription key, you shouldn't need to change
-        // this region.
+       
         var uriBase =
             "https://southeastasia.api.cognitive.microsoft.com/vision/v2.0/analyze";
  
